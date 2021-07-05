f6ef094fe9994ed33c57077d8a00b6ab308203ad
Claude Denis Glauser <claudedenis.glauser@post.ch>
2021-07-05 10:48:01 +0200

    unit test for interacting with git.

---
+++ AdHocTestingEnvironmentsTests/GitTest.cs
@@ -0,0 +1,109 @@
+﻿using LibGit2Sharp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdHocTestingEnvironmentsTests
+{
+    public class GitTest
+    {
+        [Fact]
+        public async Task TestGit()
+        {
+            IConfiguration configuration = GetConfiguration();
+            string localPath = configuration.GetValue<string>("path");
+            string gitUrl = configuration.GetValue<string>("giturl");
+            string gitUser = configuration.GetValue<string>("gituser");
+            string gitPassword = configuration.GetValue<string>("gitpwd");
+
+
+            string path = $"{localPath}{Path.DirectorySeparatorChar}gittest";
+
+            Repository.Clone(gitUrl, path);
+
+            //Create a file
+            Guid guid = Guid.NewGuid();
+            string guidString = guid.ToString();
+            string newFileFileName = $"{guid}.txt";
+            string fileNameBase = "base.txt";
+
+            await File.WriteAllTextAsync($"{path}{Path.DirectorySeparatorChar}{newFileFileName}", guidString);
+            await File.AppendAllTextAsync($"{path}{Path.DirectorySeparatorChar}{fileNameBase}", $"\n{guidString}");
+
+            //Update a file
+
+
+            using (Repository repo = new Repository(path))
+            {
+                StageChanges(repo, newFileFileName);
+                StageChanges(repo, fileNameBase);
+                CommitChanges(repo);
+                PushChanges(repo, gitUser, gitPassword);
+            }
+
+            ForceDeleteDirectory(path);
+        }
+
+        public void StageChanges(Repository repo, string path)
+        {
+            Commands.Stage(repo, path);
+        }
+
+        public void CommitChanges(Repository repo)
+        {
+            Signature signature = new Signature("larrylaffer", "larrylaffer@sierra.com", DateTimeOffset.Now);
+            {
+
+            };
+
+            repo.Commit("egotistic commit", signature, signature);
+        }
+
+        public void PushChanges(Repository repo, string userName, string pw)
+        {
+            var remote = repo.Network.Remotes["origin"];
+            var options = new PushOptions()
+            {
+                CredentialsProvider = (_user, _valid, _hostname) =>
+                {
+                    return new UsernamePasswordCredentials { Username = userName, Password = pw, };
+                },
+            };
+
+            Signature signature = new Signature("larrylaffer", "larrylaffer@sierra.com", DateTimeOffset.Now);
+            {
+
+            };
+
+            // options.Credentials = credentials;
+            var pushRefSpec = @"refs/heads/master";
+
+            repo.Network.Push(remote, pushRefSpec, options);
+        }
+
+        private IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<KubernetesClientServiceTest>();
+
+            return builder.Build();
+        }
+
+        private static void ForceDeleteDirectory(string path)
+        {
+            var directory = new DirectoryInfo(path) { Attributes = FileAttributes.Normal };
+
+            foreach (var info in directory.GetFileSystemInfos("*", SearchOption.AllDirectories))
+            {
+                info.Attributes = FileAttributes.Normal;
+            }
+
+            directory.Delete(true);
+        }
+    }
+}
---
+++ AdHocTestingEnvironmentsTests/YamlTest.cs
@@ -0,0 +1,126 @@
+﻿using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdHocTestingEnvironmentsTests
+{
+    public class YamlTest
+    {
+        [Fact]
+        void SaveAsYaml()
+        {
+            V1Deployment deployment = CreateDeployment("sfsd", "dsfds");
+            string configMapAsString = Yaml.SaveToString(deployment);
+        }
+
+
+      
+
+        private V1Deployment CreateDeployment(string appName, string image)
+        {
+            // Deployment
+            var deployment = CreateDeploymentHeader(appName);
+
+            // Deployment Volume
+            deployment.Spec.Template.Spec.Volumes = new List<V1Volume>();
+            var volume = CreateVolumeForPlsqlContainer(appName);
+            deployment.Spec.Template.Spec.Volumes.Add(volume);
+
+            var appContainer = CreateAppContainer(appName, image);
+            deployment.Spec.Template.Spec.Containers.Add(appContainer);
+
+            var psqlContainer = CreatePsqlContainer(appName);
+            deployment.Spec.Template.Spec.Containers.Add(psqlContainer);
+
+            return deployment;
+        }
+
+        private V1Deployment CreateDeploymentHeader(string appName)
+        {
+            var deployment = new V1Deployment();
+            deployment.Metadata = new V1ObjectMeta();
+            deployment.Metadata.Name = appName;
+            deployment.Metadata.Labels = new Dictionary<string, string>();
+            deployment.Metadata.Labels["creator"] = "my";
+            deployment.Spec = new V1DeploymentSpec();
+            deployment.Spec.Selector = new V1LabelSelector();
+            deployment.Spec.Selector.MatchLabels = new Dictionary<string, string>();
+            deployment.Spec.Selector.MatchLabels["app"] = appName;
+            deployment.Spec.Template = new V1PodTemplateSpec();
+            deployment.Spec.Template.Metadata = new V1ObjectMeta();
+            deployment.Spec.Template.Metadata.Labels = new Dictionary<string, string>();
+            deployment.Spec.Template.Metadata.Labels["app"] = appName;
+            deployment.Spec.Template.Spec = new V1PodSpec();
+            deployment.Spec.Template.Spec.Containers = new List<V1Container>();
+            return deployment;
+        }
+
+        private V1Volume CreateVolumeForPlsqlContainer(string appName)
+        {
+            var volume = new V1Volume();
+            volume.Name = "initscript";
+            volume.ConfigMap = new V1ConfigMapVolumeSource();
+            volume.ConfigMap.Name = appName;
+            return volume;
+        }
+
+        private V1Container CreateAppContainer(string appName, string image)
+        {
+            var appContainer = new V1Container();
+            appContainer.Name = appName;
+            appContainer.Image = image;
+            appContainer.Env = new List<V1EnvVar>();
+            appContainer.Env.Add(new V1EnvVar()
+            {
+                Name = "PathBase",
+                Value = $"endpoint/{appName}",
+            });
+            appContainer.Env.Add(new V1EnvVar()
+            {
+                Name = "ConnectionStrings__SampleWebAppContext",
+                Value = $"Server=localhost;Port=5432;Database=test;User Id=postgres;Password=verysecret",
+            });
+            appContainer.Env.Add(new V1EnvVar()
+            {
+                Name = "DatabaseTech",
+                Value = $"NpgSql",
+            });
+            return appContainer;
+        }
+
+        private V1Container CreatePsqlContainer(string appName)
+        {
+            var psqlContainer = new V1Container();
+            psqlContainer.Name = $"{appName}psql";
+            psqlContainer.Image = "postgres";
+            psqlContainer.Env = new List<V1EnvVar>();
+            psqlContainer.Env.Add(new V1EnvVar()
+            {
+                Name = "POSTGRES_PASSWORD",
+                Value = $"verysecret",
+            });
+            psqlContainer.VolumeMounts = new List<V1VolumeMount>();
+            psqlContainer.VolumeMounts.Add(new V1VolumeMount()
+            {
+                Name = "initscript",
+                MountPath = "/docker-entrypoint-initdb.d",
+            });
+            return psqlContainer;
+        }
+
+        private V1ConfigMap CreateConfigMap(string appName, string initScript)
+        {
+            var configMap = new V1ConfigMap();
+            configMap.Metadata = new V1ObjectMeta();
+            configMap.Metadata.Name = appName;
+            configMap.Data = new Dictionary<string, string>();
+            configMap.Data["script.sql"] = initScript;
+            return configMap;
+        }
+    }
+}
